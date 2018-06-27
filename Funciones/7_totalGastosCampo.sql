c8531759690f556eba0309fa245b921cdf08b2ae
DianaSolano1 <dianasolano1110@gmail.com>
2018-06-27 17:59:37 -0500

    En serio no entiendo el error

--- Funciones/7_totalGastosCampo.sql
+++ Funciones/7_totalGastosCampo.sql
@@ -4,11 +4,12 @@ as begin
 	declare @valor float
 
 
-	set @valor = (select (co.valor * (co.dedicacion / 100) * co.tiempo_obra)
+	set @valor = (
+				select 
+					(co.valor * (co.dedicacion / 100) * co.tiempo_obra)
 				from t_gastos_campos_oficinas co
 					LEFT JOIN t_AIU aiu ON co.id_AIU = aiu.id
-					LEFT JOIN t_cliente c ON aiu.id_cliente = c.ID
-					
+					LEFT JOIN t_cliente c ON aiu.id_cliente = c.ID	
 			);
 
 	return @valor
